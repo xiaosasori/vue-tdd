63d8e9d987e4c5fcbd248c8d3bcc03d5d2675b96
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-04-22 16:58:28 +0900

    chore: update config for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
